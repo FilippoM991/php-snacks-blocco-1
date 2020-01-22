98ceaac018c333b9e807bbe0f956881e1a763471
Maran Filippo <filippo.maran.fm91@gmail.com>
2020-01-22 18:08:52 +0100

    snack 5

---
+++ php_snack_5/index.php
@@ -0,0 +1,10 @@
+<!-- Creare un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il
+paragrafo e suddividerlo in tanti paragrafi: ogni volta che si incontra un punto,
+bisogna creare un nuovo paragrafo. -->
+<?php
+$paragrafo_lungo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+ $array_paragrafo = explode(".", $paragrafo_lungo );
+ echo "<pre>";
+ var_dump($array_paragrafo);
+ echo "</pre>"
+ ?>
